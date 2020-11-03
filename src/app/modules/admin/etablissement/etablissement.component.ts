917779da7935868058e47e974e8eadcba224372d
eva <evadoe@example.com>
2020-11-03 23:40:34 +0000

    Amina dafa rew,bayil nelaw

--- src/app/modules/admin/etablissement/etablissement.component.ts
+++ src/app/modules/admin/etablissement/etablissement.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-etablissement',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./etablissement.component.css']
 })
 export class EtablissementComponent implements OnInit {
+  displayedColumns: string[] = ['nomEtablissement', 'filiere', 'contact', 'adresse', 'action'];
+  source: MatTableDataSource<any>;
+  data = [
+    {nomEtablissement: 'ESP', filiere: 'DORMIR', contact: '33-854-58-52', adresse: 'DAKAR'},
+    {nomEtablissement: 'EPT', filiere: 'REVEILLE', contact: '33-854-58-52', adresse: 'DAKAR'},
+    {nomEtablissement: 'IAM', filiere: 'SOMNOLE', contact: '33-854-58-52', adresse: 'DAKAR'},
 
-  constructor() { }
+  ];
+  constructor(private dialog: MatDialog,) { }
 
   ngOnInit(): void {
   }
